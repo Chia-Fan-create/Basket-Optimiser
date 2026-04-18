e7bdf8b94ba7d52187a03f24ede669103e431ba7
Johnson Jao <johnson00111usa2@gmail.com>
2026-04-18 17:34:41 -0400

    feat: include default_consumption_days_per_unit in products query

--- db/queries/products.sql
+++ db/queries/products.sql
@@ -4,7 +4,8 @@
 
 -- name: get_all_with_category
 SELECT p.product_id AS id, p.name,
-       c.name AS category
+       c.name AS category,
+       p.default_consumption_days_per_unit
 FROM products p
 LEFT JOIN categories c ON p.category_id = c.category_id
 ORDER BY p.product_id;
